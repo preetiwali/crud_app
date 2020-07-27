72f3792672485503380af4f7f4f71031cc3d76b1
preetiwali <preetiwali212@gmail.com>
2020-07-28 00:29:41 +0400

    testing user render

--- src/UserCrud/GetUser.js
+++ src/UserCrud/GetUser.js
@@ -17,16 +17,30 @@ class UserList extends React.Component{
     } 
   
     componentDidMount(){  
-       axios.get(`https://cors-anywhere.herokuapp.com/${apiUrl}/GetUserDetails`,  {data: { 'Content-Type': 'application/json', "origin,X-Requested-With": "XMLHttpRequest" }}).then(response => response.data).then(  
-            (result)=>{  
-                this.setState({  
-                    users:result  
-                });  
-            },  
-            (error)=>{  
-                this.setState({error});  
-            }  
-        ) 
+      //  axios.get(`https://cors-anywhere.herokuapp.com/${apiUrl}/GetUserDetails`,  {data: { 'Content-Type': 'application/json', "origin,X-Requested-With": "XMLHttpRequest" }}).then(response => response.data).then(  
+      //       (result)=>{  
+      //           this.setState({  
+      //               users:result  
+      //           });  
+      //       },  
+      //       (error)=>{  
+      //           this.setState({error});  
+      //       }  
+      //   )
+      var config = {
+        method: 'get',
+        url: 'http://localhost:55537/api/User',
+        headers: { }
+      };
+      
+      axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+       
     }  
   
       
@@ -42,7 +56,14 @@ class UserList extends React.Component{
     }  
   
     render(){         
-        const{error,users}=this.state;  
+        let {error,users}=this.state;
+        users = [{
+          user_id: "1",
+          user_name: "test",  
+          user_email: "test@gmail.com",  
+          user_phone: "976343526211",  
+          location_id: "R&D"
+        }]
         if(error){  
             return(  
                 <div>Error:{error.message}</div>  
@@ -70,7 +91,7 @@ class UserList extends React.Component{
                           <td>{user.user_email}</td>  
                           <td>{user.user_phone}</td>  
                           <td>{user.location_id}</td>   
-                          <td><Button variant="info" onClick={() => this.props.editUser(user.user_id)}>Edit</Button>       
+                          <td><Button variant="info"  onClick={() => this.props.editUser(user.user_id)}>Edit</Button>       
                           <Button variant="danger" onClick={() => this.deleteUser(user.user_id)}>Delete</Button>  
                           </td>  
                         </tr>  
